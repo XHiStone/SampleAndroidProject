0c68afb8182e3915b67a8257c8d36437a31e8869
18337129968 <295233477@qq.com>
2016-12-09 10:28:21 +0800

    2016年12月1日 14:36:36

--- app/src/main/java/com/app/sampleandroidproject/utils/BaseTools.java
+++ app/src/main/java/com/app/sampleandroidproject/utils/BaseTools.java
@@ -3,9 +3,11 @@ package com.app.sampleandroidproject.utils;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.support.annotation.DrawableRes;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import java.text.DecimalFormat;
 import java.util.regex.Matcher;
@@ -97,27 +99,22 @@ public class BaseTools {
         return "0";
     }
 
-
-    /**
-     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
-     */
-    public static int dip2px(Context context, float dpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
-    }
-
-    /**
-     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
-     */
-    public static int px2dip(Context context, float pxValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (pxValue / scale + 0.5f);
-    }
-
     /**
      * 价钱单位换算
      */
     public static String getPrice(int price) {
         return price / 100 + "." + (price % 100 == 0 ? "00" : (price % 100 < 10 ? ("0" + price % 100) : price % 100));
+    }
+
+    public static ImageView getItemImageView(Context context, @DrawableRes int placeholderResId) {
+        return getItemImageView(context, placeholderResId, ImageView.ScaleType.CENTER_CROP);
+    }
+
+    public static ImageView getItemImageView(Context context, @DrawableRes int placeholderResId, ImageView.ScaleType scaleType) {
+        ImageView imageView = new ImageView(context);
+        imageView.setImageResource(placeholderResId);
+        imageView.setClickable(true);
+        imageView.setScaleType(scaleType);
+        return imageView;
     }
 }
